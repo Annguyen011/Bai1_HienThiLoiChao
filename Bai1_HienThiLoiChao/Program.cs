45a991ecdb051674430c840025238c49a135f2c7
Anngu <Anngu@MSI>
2023-03-30 23:49:03 +0700

    Add project files.

---
+++ Bai1_HienThiLoiChao/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bai1_HienThiLoiChao
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string a;
+            Console.WriteLine("Enter your name: ");
+            a = Console.ReadLine();
+            Console.WriteLine($"Hello: {a}");
+            Console.ReadLine();
+        }
+    }
+}
